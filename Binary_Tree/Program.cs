5ab8977f257b9653b287684870fd62895f6b1d4b
OwenX-id <choubao17@yahoo.com>
2026-03-12 15:44:50 +0000

    files

---
+++ Binary_Tree/Program.cs
@@ -0,0 +1,25 @@
+﻿public class BinaryTree
+{
+    TreeNode _root;
+
+    public BinaryTree()
+    {
+        _root = null;
+    }
+}
+
+internal class TreeNode
+{
+    int _value;
+    TreeNode _left;
+    TreeNode _right;
+
+    internal TreeNode(int v)
+    {
+        _value = v;
+        _left = null;
+        _right = null;
+    }
+}
+
+
